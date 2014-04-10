c52856b49cf7042b9352af70ab1f4f7be58e49ff
Adele Kyriacou <adelekyriacou@gmail.com>
2014-04-10 18:32:00 +0100

    Implement `Deck.build()`

--- 02-cards-stacked-against-you/lib/deck.js
+++ 02-cards-stacked-against-you/lib/deck.js
@@ -3,11 +3,27 @@
 // Create a deck and deal from it.
 //
 
-// Helper functions
-//
-
-// Main export is the Adele object
+// Main export is the Deck object
 //
 var Deck = {};
 
+Deck.suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds'];
+Deck.values = 'A,2,3,4,5,6,7,8,9,10,J,Q,K'.split(',');
+
+Deck.build = function () {
+  return Deck.suits.reduce(addSuitToDeck, []);
+};
+
 module.exports = Deck;
+
+// Helper functions
+//
+function buildSuit(suit) {
+  return Deck.values.map(function (value) {
+    return [suit, value];
+  });
+}
+
+function addSuitToDeck(deck, suit) {
+  return deck.concat(buildSuit(suit));
+}
--- 02-cards-stacked-against-you/test/deckSpec.js
+++ 02-cards-stacked-against-you/test/deckSpec.js
@@ -2,8 +2,14 @@ var Deck = require('../lib/deck');
 
 describe('Deck', function () {
   describe('.build()', function () {
-    it('should contain the Ace of Spades', function () {
-      expect(Deck.build()).toContain(['Spades', 'A']);
+    it('should contain a full deck', function () {
+      var deck = Deck.build();
+
+      ['Spades', 'Hearts', 'Clubs', 'Diamonds'].forEach(function (suit) {
+        Deck.values.forEach(function (value) {
+          expect(deck).toContain([suit, value]);
+        });
+      });
     });
   });
 });
