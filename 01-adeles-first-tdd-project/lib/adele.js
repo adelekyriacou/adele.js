8a8ea239652013c4e3a9552b082f2852f5fb1bbc
Adele Kyriacou <adelekyriacou@gmail.com>
2014-06-24 18:06:19 +0100

    Complete Adele's first TDD Project

--- 01-adeles-first-tdd-project/lib/adele.js
+++ 01-adeles-first-tdd-project/lib/adele.js
@@ -3,18 +3,28 @@
 // This module contains the core logic of our project.
 //
 
+// Main export is the Adele object
+//
+var Adele = {};
+
+Adele.randomCard = function () {
+  return [randomSuit(), randomValue()];
+};
+
+module.exports = Adele;
+
 // Helper functions
 //
 function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Main export is the Adele object
-//
-var Adele = {};
+function randomSuit() {
+  var suits = ['hearts', 'spades', 'diamonds', 'clubs'];
+  return suits[randomInt(0, 3)];
+}
 
-Adele.random = function () {
-  return randomInt(1, 13);
-};
-
-module.exports = Adele;
+function randomValue() {
+  var values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+  return values[randomInt(0, 12)];
+}
--- 01-adeles-first-tdd-project/test/adeleSpec.js
+++ 01-adeles-first-tdd-project/test/adeleSpec.js
@@ -1,13 +1,21 @@
 var Adele = require('../lib/adele');
 
 describe('Adele', function () {
-  describe('.random()', function () {
-    it('should return a random number > 0', function () {
-      expect(Adele.random()).toBeGreaterThan(0);
+  describe('.randomCard()', function () {
+    it('should return an array of length 2', function () {
+      expect(Adele.randomCard().length).toEqual(2);
     });
 
-    it('should return a random number < 13', function () {
-      expect(Adele.random()).toBeLessThan(14);
+    it('should return an array whose first value is a valid suit', function () {
+      var validSuits = ['hearts', 'spades', 'diamonds', 'clubs'];
+      var card = Adele.randomCard();
+      expect(validSuits).toContain(card[0]);
+    });
+
+    it('should return an array whose second value is a valid card value', function () {
+      var validValues = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+      var card = Adele.randomCard();
+      expect(validValues).toContain(card[1]);
     });
   });
 });
